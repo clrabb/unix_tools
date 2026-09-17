13ae5e583d16ea45ccae0d73ce33aa9ff0d2a077
Chuck Rabb <chuck.rabb@gmail.com>
2026-09-17 19:50:14 -0400

    First version of freq

---
+++ freq.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+
+lines = {}
+
+for cnt, ln in enumerate( sys.stdin ):
+    line = ln.strip()
+
+    if line in lines:
+        cnt = lines[ line ]
+        cnt += 1
+        lines[ line ] = cnt
+    else:
+        lines[ line ] = 1
+
+
+for line in lines:
+    cnt = lines[ line ]
+    print( f"{line}\t{cnt}" )
